13c5719d708293920a0c36e9d06638459839a841
Darappa Badole <120586187+DarappaBadole@users.noreply.github.com>
2023-04-02 18:59:06 +0530

    Add files via upload
    First angular project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'StringInterpolation'`, () => {
+  it(`should have as title 'FirstProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('StringInterpolation');
+    expect(app.title).toEqual('FirstProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('StringInterpolation app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FirstProject app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
+import { MarvellousComponent } from './marvellous/marvellous.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    MarvellousComponent
   ],
   imports: [
     BrowserModule
